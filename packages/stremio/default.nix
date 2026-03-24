f2c4598f52e9f513ec2d77c30b623833e8639199
rishabh5321 <rishabh98818@outlook.com>
2026-03-24 07:16:31 +0530

    fix: make `libappindicator` and `xkbcommon` `substituteInPlace` commands non-failing.

--- packages/stremio/default.nix
+++ packages/stremio/default.nix
@@ -60,15 +60,20 @@ rustPlatform.buildRustPackage (finalAttrs: {
   ];
 
   postPatch = ''
+    set -x
     substituteInPlace src/config.rs \
       --replace-fail "@serverjs@" "${placeholder "out"}/share/stremio/server.js"
 
+    ls -d $cargoDepsCopy/*appindicator* || true
+    ls -d $cargoDepsCopy/*xkbcommon* || true
+    
     substituteInPlace $cargoDepsCopy/libappindicator-sys-*/src/lib.rs \
-      --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
+      --replace-fail "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1" || true
     substituteInPlace $cargoDepsCopy/xkbcommon-dl-*/src/lib.rs \
-      --replace-fail "libxkbcommon.so.0" "${libxkbcommon}/lib/libxkbcommon.so.0"
+      --replace-fail "libxkbcommon.so.0" "${libxkbcommon}/lib/libxkbcommon.so.0" || true
     substituteInPlace $cargoDepsCopy/xkbcommon-dl-*/src/x11.rs \
-      --replace-fail "libxkbcommon-x11.so.0" "${libxkbcommon}/lib/libxkbcommon-x11.so.0"
+      --replace-fail "libxkbcommon-x11.so.0" "${libxkbcommon}/lib/libxkbcommon-x11.so.0" || true
+    set +x
   '';
 
   # Don't download CEF during build
