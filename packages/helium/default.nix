63ed89af25cb48209e0dacbc5ed15c9307993e83
rishabh5321 <rishabh98818@outlook.com>
2026-04-20 07:49:52 +0530

    feat(helium): add package and update script
    Adds a Nix package for Helium and an update script to automate version checks and hash calculation.

---
+++ packages/helium/default.nix
@@ -0,0 +1,41 @@
+{ lib
+, fetchurl
+, appimageTools
+}:
+let
+  pname = "helium";
+  version = "0.11.3.2";
+  src = fetchurl {
+    url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
+    hash = "sha256-5gdyKg12ZV2hpf0RL+eoJnawuW/J8NobiG+zEA0IOHA=";
+  };
+
+  appimageContents = appimageTools.extractType2 { inherit pname src version; };
+in
+appimageTools.wrapType2 {
+  inherit pname src version;
+
+  extraInstallCommands = ''
+    install -Dm644 ${appimageContents}/helium.desktop \
+      $out/share/applications/helium.desktop
+
+    # Try to install icon if it exists
+    if [ -f ${appimageContents}/helium.png ]; then
+      install -Dm644 ${appimageContents}/helium.png \
+        $out/share/icons/hicolor/512x512/apps/helium.png
+    fi
+
+    substituteInPlace $out/share/applications/helium.desktop \
+      --replace-fail 'Exec=helium' 'Exec=${placeholder "out"}/bin/helium'
+  '';
+
+  meta = {
+    description = "Helium - A simple and modern way to watch anime";
+    homepage = "https://github.com/imputnet/helium-linux";
+    license = lib.licenses.gpl3Only;
+    maintainers = with lib.maintainers; [ ];
+    mainProgram = "helium";
+    platforms = lib.platforms.linux;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+  };
+}
